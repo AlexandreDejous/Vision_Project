ab1221f8eb9f2227e57811f8f9ce637a1d43b83f
AlexandreDejous <alex.dejous@gmail.com>
2019-06-17 19:06:38 +0200

    clean main folder

--- LoG.m
+++
@@ -1,4 +0,0 @@
-function im = LoG(im)
-h = fspecial('log',4,3)*10000;
-im = imfilter(im,h);
-end
--- adjust_luminosity.m
+++
@@ -1,6 +0,0 @@
-function result = adjust_luminosity(im)
-[sizex, sizey, sizez] = size(im);
-imInv = imcomplement(im);
-imInv2 = imreducehaze(imInv);
-result = imcomplement(imInv2);
-end
